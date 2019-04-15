a647bfe9c6621932394cbef27f9cd87ea7acdc15
Kirk Clawson <kirk.clawson@outlook.com>
2019-04-14 23:27:08 -0400

    Add esri-loader dep and hook it into module init

--- modules/arc-ng/src/lib/arc-ng.module.ts
+++ modules/arc-ng/src/lib/arc-ng.module.ts
@@ -1,5 +1,20 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, InjectionToken, ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { ArcNgComponent } from './arc-ng.component';
+import * as esriLoader from 'esri-loader';
+import { ILoadScriptOptions } from 'esri-loader';
+
+const loaderToken = new InjectionToken<ILoadScriptOptions>('ILoadScriptOptions');
+
+export function init(config: ILoadScriptOptions) {
+  return async () => {
+    try {
+      await esriLoader.loadScript(config);
+      console.log('Main ArcGIS script loaded');
+    } catch (e) {
+      console.error('There was an error during the Esri Api bootstrapping process', e);
+    }
+  };
+}
 
 @NgModule({
   declarations: [ArcNgComponent],
@@ -7,4 +22,21 @@ import { ArcNgComponent } from './arc-ng.component';
   ],
   exports: [ArcNgComponent]
 })
-export class ArcNgModule { }
+export class ArcNgModule {
+
+  constructor(@Optional() @SkipSelf() parentModule: ArcNgModule) {
+    if (parentModule) {
+      throw new Error('ArcNgModule is already loaded. Import it in the AppModule only');
+    }
+  }
+
+  static forRoot(options?: ILoadScriptOptions): ModuleWithProviders {
+    return {
+      ngModule: ArcNgModule,
+      providers: [
+        { provide: loaderToken, useValue: options },
+        { provide: APP_INITIALIZER, useFactory: init, multi: true, deps: [loaderToken] }
+      ]
+    };
+  }
+}
