5ed2167a29fea8fc722e2825cdc9f5b348b3fb68
Kirk Clawson <kirk.clawson@outlook.com>
2019-04-21 21:18:58 -0400

    Load order for content child fixed

--- modules/arc-ng/src/lib/components/label-class/label-class.component.ts
+++ modules/arc-ng/src/lib/components/label-class/label-class.component.ts
@@ -1,29 +1,29 @@
-import { Component, ContentChild, Input } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, Input } from '@angular/core';
 import { EsriAsyncComponentBase } from '../../shared/esri-component-base';
 import { LayerComponentBase } from '../../shared/layer-component-base';
 import { LayerType, PointLabelPlacement, PolygonPointPlacement, PolylineLabelPlacement } from '../../shared/enums';
 import { createCtorParameterObject, loadEsriModules } from '../../shared/utils';
 import { TextSymbolDirective } from '../../directives/features/text-symbol.directive';
 
-// const DEFAULT_SYMBOL = {
-//   type: 'text',  // autocasts as new TextSymbol()
-//   color: 'white',
-//   haloColor: 'black',
-//   haloSize: '1px',
-//   xoffset: 3,
-//   yoffset: 3,
-//   font: {  // autocast as new Font()
-//     size: 12,
-//     family: 'sans-serif',
-//     weight: 'bold'
-//   }
-// };
+const DEFAULT_SYMBOL = {
+  type: 'text',  // auto casts as new TextSymbol()
+  color: 'white',
+  haloColor: 'black',
+  haloSize: '1px',
+  xoffset: 3,
+  yoffset: 3,
+  font: {  // auto cast as new Font()
+    size: 12,
+    family: 'sans-serif',
+    weight: 'bold'
+  }
+};
 
 @Component({
   selector: 'label-class',
   template: '<ng-content></ng-content>'
 })
-export class LabelClassComponent extends EsriAsyncComponentBase<__esri.LabelClass> {
+export class LabelClassComponent extends EsriAsyncComponentBase<__esri.LabelClass> implements AfterContentInit {
   @Input()
   set expression(value: string) {
     if (this.__expression !== value) {
@@ -50,10 +50,10 @@ export class LabelClassComponent extends EsriAsyncComponentBase<__esri.LabelClas
   set minScale(value: number) {
     this.setField('minScale', value);
   }
-  // @Input()
-  // set symbol(value: __esri.TextSymbolProperties | __esri.LabelSymbol3DProperties) {
-  //   this.setAutoCastField('symbol', value);
-  // }
+  @Input()
+  set symbol(value: __esri.TextSymbolProperties | __esri.LabelSymbol3DProperties) {
+    this.setAutoCastField('symbol', value);
+  }
   @Input()
   set useCodedValues(value: boolean) {
     this.setField('useCodedValues', value);
@@ -67,27 +67,31 @@ export class LabelClassComponent extends EsriAsyncComponentBase<__esri.LabelClas
   private __expression: string;
   private readonly parentLayerType: LayerType;
 
-  @ContentChild(TextSymbolDirective) set textChild(value: TextSymbolDirective) {
-    this.temp = value;
-    const textInstance = value.createInstance();
-    console.log('Text Instance created', textInstance);
-    this.setAutoCastField('symbol', textInstance);
+  @ContentChild(TextSymbolDirective)
+  set textChild(value: TextSymbolDirective) {
+    this.symbolComponent = value;
   }
+  private symbolComponent: TextSymbolDirective;
 
   constructor(parent: LayerComponentBase<__esri.Layer>) {
     super();
     this.parentLayerType = parent.layerType;
   }
 
-  private temp: TextSymbolDirective;
+  ngAfterContentInit(): void {
+    if (this.symbolComponent != null) {
+      this.setAutoCastField('symbol', this.symbolComponent.createInstance());
+    }
+  }
 
   async createInstance(): Promise<__esri.LabelClass> {
+    console.log('CI');
     type modules = [typeof import ('esri/layers/support/LabelClass')];
     const [ LabelClass ] = await loadEsriModules<modules>(['esri/layers/support/LabelClass']);
     const params = createCtorParameterObject<__esri.LabelClassProperties>(this);
-    // if (params.symbol == null) {
-    params.symbol = this.temp.createInstance();
-    // }
+    if (params.symbol == null) {
+      params.symbol = DEFAULT_SYMBOL;
+    }
     this.instance = new LabelClass(params);
     return this.instance;
   }
